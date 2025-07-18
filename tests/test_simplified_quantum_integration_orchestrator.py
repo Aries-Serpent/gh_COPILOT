2209ae234cb5b3d0f58bfa78eff2b69e20f8282c
Statix <marc.baetiong@gmail.com>
2025-07-18 13:27:48 -0500

    Update tests to import from main modules

--- tests/test_simplified_quantum_integration_orchestrator.py
+++ tests/test_simplified_quantum_integration_orchestrator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from archive.consolidated_scripts.simplified_quantum_integration_orchestrator import EnterpriseUtility
+from simplified_quantum_integration_orchestrator import EnterpriseUtility
 import logging
 
 
