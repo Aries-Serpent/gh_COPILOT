2e8ea666629a3cc282b73b405e835223a9874ef9
Statix <marc.baetiong@gmail.com>
2025-07-21 20:46:17 -0500

    fix: clean up unused imports

---
+++ copilot_qiskit_stubs/__init__.py
@@ -0,0 +1,5 @@
+"""Lightweight Qiskit stubs used for testing.
+
+These stubs provide minimal implementations of select Qiskit modules so that
+tests can run without the real Qiskit package installed.
+"""
