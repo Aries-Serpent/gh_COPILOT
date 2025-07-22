15964eb87757ee609748f633271e2f4f61aa97a0
Statix <marc.baetiong@gmail.com>
2025-07-21 20:53:58 -0500

    feat: expose documentation manager in package

---
+++ documentation/__init__.py
@@ -0,0 +1,10 @@
+"""Enterprise project documentation package.
+
+Provides tools for generating and managing documentation assets. The
+:class:`~documentation.enterprise_documentation_manager.EnterpriseDocumentationManager`
+class is re-exported here for convenience.
+"""
+
+from .enterprise_documentation_manager import EnterpriseDocumentationManager
+
+__all__ = ["EnterpriseDocumentationManager"]
