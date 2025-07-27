7d9cd62699ec0f4e3c85e358cde9a9642f2c18ef
Statix <marc.baetiong@gmail.com>
2025-07-26 21:03:12 -0500

    feat: add validator and progress bar to quantum orchestrator

--- tests/test_qubo_integration.py
+++ tests/test_qubo_integration.py
@@ -3,6 +3,19 @@ import logging
 
 from scripts.automation.quantum_integration_orchestrator import integrate_qubo_problems
 
+
+class DummyTqdm:
+    """Minimal iterable tqdm replacement for testing."""
+
+    def __init__(self, iterable, *args, **kwargs):
+        self.iterable = list(iterable)
+        self.updates = 0
+
+    def __iter__(self):
+        for item in self.iterable:
+            self.updates += 1
+            yield item
+
 logging.getLogger().setLevel(logging.CRITICAL)
 
 
@@ -14,3 +27,23 @@ def test_integrate_qubo_problems():
     solution, energy = integrate_qubo_problems(qubos)
     assert solution == [1, 1]
     assert energy == -2.0
+
+
+def test_progress_bar(monkeypatch):
+    qubos = [
+        [[1.0, -2.0], [-2.0, 1.0]],
+        [[2.0, -1.0], [-1.0, 2.0]],
+    ]
+
+    bars = []
+
+    def dummy_tqdm(iterable, *args, **kwargs):
+        bar = DummyTqdm(iterable, *args, **kwargs)
+        bars.append(bar)
+        return bar
+
+    monkeypatch.setattr(
+        "scripts.automation.quantum_integration_orchestrator.tqdm", dummy_tqdm
+    )
+    integrate_qubo_problems(qubos)
+    assert bars and bars[0].updates == len(qubos)
