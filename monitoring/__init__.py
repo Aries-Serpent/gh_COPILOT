be4074a82f000ed607447b60fbbfdb81a0dbe7db
Statix <marc.baetiong@gmail.com>
2025-08-09 01:05:24 -0500

    feat: add anomaly detection and session lifecycle metrics

--- monitoring/__init__.py
+++ monitoring/__init__.py
@@ -1,5 +1,6 @@
 """Monitoring package."""
 
 from .service_health import check_service, run_health_checks, SERVICES
+from .baseline_anomaly_detector import BaselineAnomalyDetector
 
-__all__ = ["check_service", "run_health_checks", "SERVICES"]
+__all__ = ["check_service", "run_health_checks", "SERVICES", "BaselineAnomalyDetector"]
